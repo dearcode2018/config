22460fcaf9a4d974f39034699025d06659d2adc8
qianye.zheng <>
2020-12-14 13:32:34 +0800

    no message

---
+++ config-util/src/test/java/com/hua/test/config/YamlToPropertiesTest.java
@@ -0,0 +1,340 @@
+/**
+ * 描述: 
+ * YamlToPropertiesTest.java
+ * 
+ * @author qye.zheng
+ *  version 1.0
+ */
+package com.hua.test.config;
+
+//静态导入
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.dataformat.javaprop.JavaPropsFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLGenerator;
+import com.fasterxml.jackson.dataformat.yaml.YAMLParser;
+import com.hua.test.BaseTest;
+import com.hua.util.ClassPathUtil;
+
+
+/**
+ * 描述: 
+ * 
+ * @author qye.zheng
+ * YamlToPropertiesTest
+ */
+//@DisplayName("测试类名称")
+//@Tag("测试类标签")
+//@Tags({@Tag("测试类标签1"), @Tag("测试类标签2")})
+public final class YamlToPropertiesTest extends BaseTest {
+    
+    private static final String ENCODING = "utf-8";
+
+    
+    /**
+     * 
+     * 描述: 
+     * @author qye.zheng
+     * 
+     */
+    //@DisplayName("test")
+    @Test
+    public void yamlToProperties() {
+        try {
+            //String path = ClassPathUtil.getClassPath("/some.yml");
+            //String path = ClassPathUtil.getClassPath("/el-admin.yml");
+            String path = ClassPathUtil.getClassPath("/boot.yml");
+            yml2Properties(path);
+            
+        } catch (Exception e) {
+            log.error("test =====> ", e);
+        }
+    }
+    
+    public static void yml2Properties(String path) {
+        final String DOT = ".";
+        List<String> lines = new LinkedList<>();
+        try {
+            YAMLFactory yamlFactory = new YAMLFactory();
+            YAMLParser parser = yamlFactory.createParser(
+                    new InputStreamReader(new FileInputStream(path), Charset.forName(ENCODING)));
+
+            String key = "";
+            String value = null;
+            JsonToken token = parser.nextToken();
+            while (token != null) {
+                if (JsonToken.START_OBJECT.equals(token)) {
+                    // do nothing
+                } else if (JsonToken.FIELD_NAME.equals(token)) {
+                    if (key.length() > 0) {
+                        key = key + DOT;
+                    }
+                    key = key + parser.getCurrentName();
+
+                    token = parser.nextToken();
+                    if (JsonToken.START_OBJECT.equals(token)) {
+                        continue;
+                    }
+                    value = parser.getText();
+                    lines.add(key + "=" + value);
+                    System.out.println(key + "=" + value);
+                    int dotOffset = key.lastIndexOf(DOT);
+                    if (dotOffset > 0) {
+                        key = key.substring(0, dotOffset);
+                    }
+                    value = null;
+                } else if (JsonToken.END_OBJECT.equals(token)) {
+                    int dotOffset = key.lastIndexOf(DOT);
+                    if (dotOffset > 0) {
+                        key = key.substring(0, dotOffset);
+                    } else {
+                        key = "";
+                        lines.add("");
+                    }
+                }
+                token = parser.nextToken();
+            }
+            parser.close();
+
+            //System.out.println(lines);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void properties2Yaml(String path) {
+        JsonParser parser = null;
+        JavaPropsFactory factory = new JavaPropsFactory();
+        try {
+            parser = factory.createParser(
+                    new InputStreamReader(new FileInputStream(path), Charset.forName(ENCODING)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            YAMLFactory yamlFactory = new YAMLFactory();
+            YAMLGenerator generator = yamlFactory.createGenerator(
+                    new OutputStreamWriter(new FileOutputStream(path), Charset.forName(ENCODING)));
+
+            JsonToken token = parser.nextToken();
+
+            while (token != null) {
+                if (JsonToken.START_OBJECT.equals(token)) {
+                    generator.writeStartObject();
+                } else if (JsonToken.FIELD_NAME.equals(token)) {
+                    generator.writeFieldName(parser.getCurrentName());
+                } else if (JsonToken.VALUE_STRING.equals(token)) {
+                    generator.writeString(parser.getText());
+                } else if (JsonToken.END_OBJECT.equals(token)) {
+                    generator.writeEndObject();
+                }
+                token = parser.nextToken();
+            }
+            parser.close();
+            generator.flush();
+            generator.close();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	//@DisplayName("test")
+	@Test
+	public void test() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("test =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("testTemp")
+	@Test
+	public void testTemp() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("testTemp=====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("testCommon")
+	@Test
+	public void testCommon() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("testCommon =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("testSimple")
+	@Test
+	public void testSimple() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("testSimple =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("testBase")
+	@Test
+	public void testBase() {
+		try {
+			
+			
+		} catch (Exception e) {
+			log.error("testBase =====> ", e);
+		}
+	}
+	
+	/**
+	 * 
+	 * 描述: [每个测试-方法]开始之前运行
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("beforeMethod")
+	@Tag(" [每个测试-方法]结束之后运行")
+	@BeforeEach
+	public void beforeMethod() {
+		System.out.println("beforeMethod()");
+	}
+	
+	/**
+	 * 
+	 * 描述: [每个测试-方法]结束之后运行
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("afterMethod")
+	@Tag(" [每个测试-方法]结束之后运行")
+	@AfterEach
+	public void afterMethod() {
+		System.out.println("afterMethod()");
+	}
+	
+	/**
+	 * 
+	 * 描述: 测试忽略的方法
+	 * @author qye.zheng
+	 * 
+	 */
+	@Disabled
+	@DisplayName("ignoreMethod")
+	@Test
+	public void ignoreMethod() {
+		System.out.println("ignoreMethod()");
+	}
+	
+	/**
+	 * 
+	 * 描述: 解决ide静态导入消除问题 
+	 * @author qye.zheng
+	 * 
+	 */
+	@DisplayName("noUse")
+	@Disabled("解决ide静态导入消除问题 ")
+	private void noUse() {
+		String expected = null;
+		String actual = null;
+		Object[] expecteds = null;
+		Object[] actuals = null;
+		String message = null;
+		
+		assertEquals(expected, actual);
+		assertEquals(message, expected, actual);
+		assertNotEquals(expected, actual);
+		assertNotEquals(message, expected, actual);
+		
+		assertArrayEquals(expecteds, actuals);
+		assertArrayEquals(expecteds, actuals, message);
+		
+		assertFalse(true);
+		assertTrue(true);
+		assertFalse(true, message);
+		assertTrue(true, message);
+		
+		assertSame(expecteds, actuals);
+		assertNotSame(expecteds, actuals);
+		assertSame(expecteds, actuals, message);
+		assertNotSame(expecteds, actuals, message);
+		
+		assertNull(actuals);
+		assertNotNull(actuals);
+		assertNull(actuals, message);
+		assertNotNull(actuals, message);
+		
+		fail();
+		fail("Not yet implemented");
+		
+		dynamicTest(null, null);
+		
+	}
+
+}
